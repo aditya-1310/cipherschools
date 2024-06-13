995aada747a81f35e28e041e11d726f044013b9c
aditya-1310 <bhadadeaditya1310@gmail.com>
2024-06-13 13:59:52 +0530

    ep17

---
+++ 2-course/Lecture-17-CipherSchool/ep17.js
@@ -0,0 +1,20 @@
+// REACT COMPONENT ARE LIKE FUNCTION 
+// they contain some logic code and they return sone UI;
+// jUST LIKE FUNCTION THEY CONTION SOME arguments that can control the UI/logic of the component
+// Render a Component is like calling a function
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+
+class App extends Component {
+  render () {
+      return (
+        <div>THIS IS NEW</div>
+      );
+  }
+}
+
+export default App;
+
+
+//This is the code of App.js//  we learned how the component works
