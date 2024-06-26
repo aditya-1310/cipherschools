d6adc79f3f41f29eac7a95b05237430ba88b6c2d
aditya-1310 <bhadadeaditya1310@gmail.com>
2024-06-26 18:52:53 +0530

    changes done

--- 3-course/Lecture-23-CipherSchool/ep23.js
+++ 3-course/Lecture-23-CipherSchool/ep23.js
@@ -39,4 +39,58 @@ const ToDoScreen = () => {
      );
    }
 export default ToDoScreen;  
+
+/*
+import { useState } from "react";
+
+const AddTask =()=>{
+    const [task, settask] = useState({
+        title: '',
+        description: '',
+    });
+
+    let handleInputChange = (e)=>{
+       settask({
+        ...task,
+        [e.target.name]: e.target.value,
+       });
+    };
+    let onformsubmit = (e)=>{
+    
+     e.preventDefault();
+      console.log(task);
+    }
+    return (
+      <div>
+          <form onClick={onformsubmit}>
+             <div className="ui form">
+    <div className="field">
+      <label>title</label>
+     
+      <input type="text"
+       placeholder="Title"
+        name="title"
+        spellCheck={false} 
+        data-ms-editor={true}
+       onChange={handleInputChange}/>
+    </div>
+          <div className="field">
+      <label>description</label>
+     
+      <textarea rows="2" 
+      placeholder="description"
+       name="description"
+       spellCheck={false} 
+       data-ms-editor={true}
+      onChange={handleInputChange}/>
+    </div>
+      </div>
+      <button className="ui primary button">Summit</button>
+  </form>
+  </div>
+    );
+  }
+  export default AddTask;
+ 
+*/
 // In this lecture learned how to put the provided data the ui with help useState 
